9e61022c681a487e19a0bf667266af5057422255
brealxbrealx <brealxbrealx@gmail.com>
2021-08-02 00:24:30 +0300

    homework2 task3

---
+++ calc.py
@@ -0,0 +1,25 @@
+# Homework2
+# Task3
+# Author: Andrey Maiboroda
+# brealxbrealx@gmail.com
+
+def add(a,b):
+    print(a+b)
+
+def sub(a,b):
+    print(a-b)
+
+def mult(a,b):
+    print(a*b) 
+
+def div(a,b):
+    print(a/b)
+
+def exp(a,b):
+    print(a**b)
+
+def mod(a,b):
+    print(a%b)
+
+def fdiv(a,b):
+    print(a//b)
